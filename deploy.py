3d0dea0ad6817d8488872ad31f33b3a9612cd137
Gene Laisne <glaisne@carbonite.com>
2022-10-13 14:12:47 -0400

    utilize parameter for config file

--- deploy.py
+++ deploy.py
@@ -1,17 +1,31 @@
-import json
 import az
+import getopt
+import json
+import sys
 from azure.identity import AzureCliCredential
 
-config_file_path = "./testing_config.json"
+CONFIG_FILE_PATH = "./testing_config.json"
 
 # import base json configuration file
-with open(config_file_path, "r") as config_base:
+with open(CONFIG_FILE_PATH, "r", encoding="utf-8") as config_base:
     config_data = config_base.read()
 
 config = json.loads(config_data)
 
 # Acquire a credential object using CLI-based authentication.
 credential = AzureCliCredential()
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hc:",["CONFIG_FILE_PATH="])
+except getopt.GetoptError:
+    print('build_data.py -c <CONFIG_FILE_PATH>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print('build_data.py -c <CONFIG_FILE_PATH>')
+        sys.exit()
+    elif opt in ("-c", "--CONFIG_FILE_PATH"):
+        CONFIG_FILE_PATH = arg
 
 
 #todo: Working here. getting everything together ot create a resoruce group and try testing it.
