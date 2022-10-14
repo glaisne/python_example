c8e27c59ac05a09b883cf9c69387eb9b2a72100a
Gene Laisne <glaisne@carbonite.com>
2022-10-14 09:42:50 -0400

    move test variables to pytest features

--- tests/infrastructure_tests/test_az_resource_group.py
+++ tests/infrastructure_tests/test_az_resource_group.py
@@ -13,20 +13,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 # import the azure module from the parent directory
 from context import az
 
-CONFIG_FILE_PATH = "./testing_config.json"
 
-# import base json configuration file
-with open(CONFIG_FILE_PATH, "r", encoding="utf-8") as config_base:
-    config_data = config_base.read()
 
-config = json.loads(config_data)
+@pytest.fixture
+def config():
+    CONFIG_FILE_PATH = "./testing_config.json"
+
+    # import base json configuration file
+    with open(CONFIG_FILE_PATH, "r", encoding="utf-8") as config_base:
+        config_data = config_base.read()
+        
+    return json.loads(config_data)
 
 # Acquire a credential object using CLI-based authentication.
-credential = AzureCliCredential()
+@pytest.fixture
+def credential():
+    return AzureCliCredential()
 
 # Obtain the management object for resources.
-resource_client = ResourceManagementClient(credential,
-                    config['subscription_id'])
+@pytest.fixture
+def client():
+    return ResourceManagementClient(credential, config['subscription_id'])
 
 def test_resource_group_name(client, config):
     '''Test that the resource group exists and has the proper name'''
@@ -37,4 +44,3 @@ def test_resource_group_location(client, config):
     '''Test that the resource group is in the correct location'''
     rg = client.resource_groups.get(config['resource_group']['name'])
     assert rg.location == config['resource_group']['locaiton']
-    
