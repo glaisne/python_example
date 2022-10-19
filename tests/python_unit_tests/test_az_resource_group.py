ce0e0b92b301cb296e131c91782d0f70d0efeb54
Gene Laisne <glaisne@carbonite.com>
2022-10-19 14:31:49 -0400

    update unit tests

--- tests/python_unit_tests/test_az_resource_group.py
+++ tests/python_unit_tests/test_az_resource_group.py
@@ -27,38 +27,40 @@ def setup_credential():
     '''pytest fixture for cli credentials'''
     return AzureCliCredential()
 
+@pytest.fixture(name="subscription_id")
+def setup_subscription_id():
+    '''pytest fixture for cli subscrption_id'''
+    return '00000000-0000-0000-0000-000000000000'
+
 @pytest.fixture(name="good_config")
 def setup_good_config():
     '''Take a valid configuration and add a generic guid subscription_id'''
     tmpconfig = json.loads(config_data)
-    tmpconfig['subscription_id'] = '00000000-0000-0000-0000-000000000000'
     return tmpconfig
 
 @pytest.fixture(name="config_with_bad_resource_group_name")
 def setup_config_with_bad_resource_group_name():
     '''take a valid configuration and modify the resource group name to be bad'''
     tmpconfig = json.loads(config_data)
-    tmpconfig['resource_group']['name'] = 'bad|name.'
-    tmpconfig['subscription_id'] = '00000000-0000-0000-0000-000000000000'
+    tmpconfig['name'] = 'bad|name.'
     return tmpconfig
 
 @pytest.fixture(name="config_with_bad_location")
 def setup_config_with_bad_location():
     '''take a valid configiruation and swap out a bad location'''
     tmpconfig = json.loads(config_data)
-    tmpconfig['resource_group']['location'] = 'bad_location'
-    tmpconfig['subscription_id'] = '00000000-0000-0000-0000-000000000000'
+    tmpconfig['location'] = 'bad_location'
     return tmpconfig
 
 
 #def create_rg_bad_rg_name(credential, good_config):
 
-def test_create_rg_bad_rg_name(credential, config_with_bad_resource_group_name):
+def test_create_rg_bad_rg_name(credential, subscription_id, config_with_bad_resource_group_name):
     '''pytest to validate the proper raise occurse with an invalid resource group name'''
     with pytest.raises(ValueError, match=' is NOT a valid resource group name'):
         az.create_resource_group(credential, config_with_bad_resource_group_name)
 
-def test_create_rg_bad_location(credential, config_with_bad_location):
+def test_create_rg_bad_location(credential, subscription_id, config_with_bad_location):
     '''pytest to validate the proper raise occurse with an invalid locaiton'''
     with pytest.raises(ValueError, match=' is NOT a valid location'):
         az.create_resource_group(credential, config_with_bad_location)
