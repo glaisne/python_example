c57e7c7fefa3583411a212b21289f0e97811db8a
Gene Laisne <glaisne@carbonite.com>
2022-10-20 11:47:29 -0400

    correct pytest for multiple resource groups

--- tests/python_unit_tests/test_az_resource_group.py
+++ tests/python_unit_tests/test_az_resource_group.py
@@ -14,35 +14,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 # import the azure module from the parent directory
 from context import az
 
-CONFIG_FILE_PATH = os.path.relpath(os.path.join(os.path.dirname(__file__), '../../config/config_az_OPSTESTING.json'))
-
-# import base json configuration file
-with open(CONFIG_FILE_PATH, "r", encoding="utf-8") as config_base:
-    config_data = config_base.read()
-
-
-
-#! todo: workout the tests if there are multiple of a resoruce, or if there are no resources of that type! 
-#! suggestion: just get the first one if there are multiple and test that.
-
-
-#def create_rg_bad_rg_name(credential, good_config):
-# rg_config = None
-# if isinstance(config['resource_group'], dict):
-#     rg_config = config['resource_group']
-# else:
-#     rg_config = config['resource_group'][0]
-
-@pytest.fixture(name="rg_config")
-def setup_rg_config():
-    '''pytest fixture for a single resource-related unit test'''
-    config = json.loads(config_data)
-    if isinstance(config['resource_group'], dict):
-        return config['resource_group']
-    else:
-        return config['resource_group'][0]
-
-
 @pytest.fixture(name="credential")
 def setup_credential():
     '''pytest fixture for cli credentials'''
@@ -53,22 +24,37 @@ def setup_subscription_id():
     '''pytest fixture for cli subscrption_id'''
     return '00000000-0000-0000-0000-000000000000'
 
-@pytest.fixture(name="good_config")
-def setup_good_config(rg_config):
-    '''Take a valid configuration and add a generic guid subscription_id'''
-    return rg_config
+@pytest.fixture(name="config_good")
+def setup_good_config():
+    '''Read the existing config file and make it good.'''
+    CONFIG_FILE_PATH = os.path.relpath(os.path.join(os.path.dirname(__file__), '../../config/config_az_OPSTESTING.json'))
 
-@pytest.fixture(name="config_with_bad_resource_group_name")
-def setup_config_with_bad_resource_group_name(rg_config):
+    # import base json configuration file
+    with open(CONFIG_FILE_PATH, "r", encoding="utf-8") as config_base:
+        config_data = config_base.read()
+
+    # do string replacement.
+    config_data = config_data.replace('<environment>', 'opstesting')
+
+    config = json.loads(config_data)
+    if isinstance(config['resource_group'], dict):
+        return config['resource_group']
+    else:
+        return config['resource_group'][0]
+
+
+
+@pytest.fixture(name="config_bad_name")
+def setup_config_with_bad_resource_group_name(config_good):
     '''take a valid configuration and modify the resource group name to be bad'''
-    tmpconfig = rg_config
+    tmpconfig = config_good
     tmpconfig['name'] = 'bad|name.'
     return tmpconfig
 
-@pytest.fixture(name="config_with_bad_location")
-def setup_config_with_bad_location(rg_config):
+@pytest.fixture(name="config_bad_location")
+def setup_config_with_bad_location(config_good):
     '''take a valid configiruation and swap out a bad location'''
-    tmpconfig = rg_config
+    tmpconfig = config_good
     tmpconfig['location'] = 'bad_location'
     return tmpconfig
 
@@ -77,12 +63,12 @@ def setup_config_with_bad_location(rg_config):
 
 
 
-def test_create_rg_bad_rg_name(credential, subscription_id, config_with_bad_resource_group_name):
+def test_create_rg_bad_rg_name(credential, subscription_id, config_bad_name):
     '''pytest to validate the proper raise occurse with an invalid resource group name'''
     with pytest.raises(ValueError, match=' is NOT a valid resource group name'):
-        az.create_resource_group(credential, subscription_id, config_with_bad_resource_group_name)
+        az.create_resource_group(credential, subscription_id, config_bad_name)
 
-def test_create_rg_bad_location(credential, subscription_id, config_with_bad_location):
+def test_create_rg_bad_location(credential, subscription_id, config_bad_location):
     '''pytest to validate the proper raise occurse with an invalid locaiton'''
     with pytest.raises(ValueError, match=' is NOT a valid location'):
-        az.create_resource_group(credential, subscription_id, config_with_bad_location)
+        az.create_resource_group(credential, subscription_id, config_bad_location)
